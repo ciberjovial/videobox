0b50e113372de92ceb79957ff22a903a13eed168
ciberjovial <ciberjovial@hotmail.com>
2015-01-16 01:36:07 -0500

    Carcasa del sistema

---
+++ funciones.php
@@ -0,0 +1,99 @@
+<?php
+/*****************************************************
+	AUTOR		:		Hugo Barraza Vizcarra  
+	CONTACTO	:		manuel_brrz@hotmail.com
+	VERSION		:      	0.1	
+*******************************************************/ 
+
+#------------------------
+#Inicio de las funciones usadas
+#-----------------------
+#Conexion db
+#------------------------
+include("includes/conexion.php");
+#-------------------------
+#Fecha
+#------------------------
+function obtenerfecha()
+{
+/*EL NUMERO DEL DIA*/
+$dia=date("d");
+
+/*OBTENGO EL STRING DEL MES*/
+$mes=date("F");
+if ($mes=="January") $mes="01"; if ($mes=="February") $mes="02";
+if ($mes=="March") $mes="03"; if ($mes=="April") $mes="04";
+if ($mes=="May") $mes="05"; if ($mes=="June") $mes="06";
+if ($mes=="July") $mes="07"; if ($mes=="August") $mes="08";
+if ($mes=="September")$mes="09"; if ($mes=="October") $mes="10";
+if ($mes=="November") $mes="11"; if ($mes=="December") $mes="12";
+
+/*FINALMENTE EL AÑO*/
+$ano=date("Y");
+
+/*RETORNAMOS LA FECHA ENTERA*/
+echo "$ano-$mes-$dia";
+}
+
+
+#-----------------
+#Comprobar sesion
+#-----------------
+function comprobar_secion(){
+//if (session_is_registered("valid_user"))
+if(!isset($_SESSION['usuario']))
+{
+		//$url=$direccion_aplicacion.'login';
+		//header("location url");
+	$mensaje="Usted no se ha Autentidentificado.";
+	header("location:login.php?msg=$mensaje");
+		die();
+}
+$user=strtoupper($_SESSION['usuario']);
+$idd=$_SESSION['codigo'];
+$cargo=$_SESSION['cargo'];
+mensaje("Bienvenido <b>$user</b> al Sistema. |<b><a href=\"cerrar.php\"> 
+			 Cerrar Sesion</a></b> | <b><a href=\"index.php\">Regresar al Inicio</a></b> | ID:$idd");
+}
+
+#--------------
+#Validar Email
+#--------------
+function validar_email($correo){
+  if (ereg("^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$",$correo))
+    return true;
+  else 
+    return false;
+}
+
+
+#-----------------------------------------
+#Realizar el nuevo ingreso de un doctor
+#-----------------------------------------
+function ingreso_doctor(){
+conectar();
+if(!empty($_GET['nomdoc']) && !empty($_GET['condoc']) 
+		  ){
+	$codemp=$_GET['codemp'];
+	$nomdoc=$_GET['nomdoc'];
+	$apedoc=$_GET['apedoc'];
+	$celdoc=$_GET['celdoc'];
+	$cordoc=$_GET['cordoc'];
+	$condoc=$_GET['condoc'];
+	$paidoc=$_GET['paidoc'];
+	$prodoc=$_GET['prodoc'];
+	$dirdoc=$_GET['dirdoc'];
+	$usedoc=$_GET['usedoc'];
+
+	mysql_query("insert into cliente values ('$codemp','$nomdoc','$apedoc','$cordoc','$paidoc','$prodoc','$dirdoc','$celdoc','$condoc','$usedoc')")or die("<b>Error1. El servidor dijo: </b> " . mysql_error());
+	echo "Se inserto el nuevo Usuario <b>$usedoc</b> correctamente";
+}
+else{
+	echo "Ingrese sus datos CORRECTAMENTE";
+}
+}
+
+#------------------------
+#Fin - Funciones
+#------------------------
+?>
---
+++ html.php
@@ -0,0 +1,97 @@
+<?php
+//echo('hola1');
+function head($estilo="estilo.css")
+{
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta name="Description" content="Sistema." />
+	<meta name="Keywords" content="Sistema" />
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<meta name="Author" content="Hugo Barraza Vizcarra- manuel_brrz@hotmail.com" />
+	<link rel="stylesheet" href="css/<?php echo $estilo ?>" type="text/css" />
+	<title>VIDEOBOX</title>
+</head>
+<body>
+<div id="header">
+  <div id="header-content">
+	<h1 id="logo-text"><img src="logo.png"><br /><a href="index.php" title=""><span></span></a></h1>
+    <h2 id="slogan">ALQUILER Y COMPRA DE VIDEOS</h2>
+    <div id="header-links">
+      <p><a href="index.php">Inicio</a> | <a href="contacto.php">Contacto</a></p>
+    </div>
+  </div>
+</div>
+<div id="nav-wrap">
+  <div id="nav">
+    <ul>
+	  <li id="current"><a href="index.php"> INICIO</a></li>
+      <li><a href="vehiculo.php"> Mi Perfil</a></li>
+	  <li><a href="catalogo.php"> Nuestro sistema</a></li>
+	  <li><a href="vercarrito.php"><img src="css/cesta.png" align="absmiddle"/> Mi pedido</a></li>
+    </ul>
+  </div>
+</div>
+<div id="content-wrap">
+<div id="content"> 	
+<?php
+}
+function menu($m=1)
+{
+if($m==0)
+{
+
+?>  
+ <div id="sidebar" >
+      <div class="sep">
+      </div>
+      <div class="sidebox">
+        <h1>Sistema </h1>
+        <p></p>
+      </div>
+ </div>
+<?
+}
+else{
+?>
+	<div id="sidebar" >
+		<div class="sep">
+       </div>
+        <h1>Promociones</h1>
+        <div class="sidebox">
+        <ul class="sidemenu">
+				<img src="css/lado.png" alt="" />
+        </div>
+		<br />
+				
+      </div>
+<?php
+}
+}
+	
+function footer()
+{
+?>
+<p></p>
+</div>
+</div>
+</div>
+<span>
+
+<div id="footer-wrap">
+  <div id="footer-bottom">
+	<p>&copy; 2015 | VIDEOBOX | Todos los derechos reservados</p>
+  </div>
+  </span>
+</div>
+</body>
+</html>
+<?php
+}
+function mensaje($mensaje="<p>Ingrese sus Nombre de Usuario y Contraseña Por favor <b><a href=\"../index.php\">Regresar a la Portada</a></b></p>")
+{
+   	echo $mensaje;
+} 
+
+?>
