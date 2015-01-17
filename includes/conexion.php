044dcb344e0a0d512082e99d7ac1bb3776732148
jimztian <jimz_21@hotmail.com>
2015-01-17 07:37:23 -0500

    Revert "Revert "registros clientes""
    This reverts commit 7d2642992f9c79a29a2e79d74f441026101aae8a.

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,8 +1,8 @@
 <?php
 function conectar()
 {
-	mysql_connect("localhost","root","123456");
-	//mysql_connect("localhost","root","13081416");  //lo cambie debido a que mi clave de base de datos es diferente , jimmy
+	//mysql_connect("localhost","root","123456");
+	mysql_connect("localhost","root","13081416");  //lo cambie debido a que mi clave de base de datos es diferente , jimmy
 	mysql_select_db("videobox");
 }
 function desconectar()
